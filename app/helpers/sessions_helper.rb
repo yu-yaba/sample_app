90d2e187bebf4f06cb9c3303311ad3eece88c515
kawai <109569162+yu-yaba@users.noreply.github.com>
2023-12-31 13:10:05 +0000

    Finish user edit, update index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,4 +24,8 @@ module SessionsHelper
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
+
+  def current_user?(user)
+    user && user == current_user
+  end
 end
